1c13a063abd86ba431b82b283533c493fbb2727f
victoireokn <Victoire.OKITODIMBA@student.hel.be>
2024-11-13 10:17:52 +0100

    ajout equipe adverse

--- examenpratique.ps1
+++ examenpratique.ps1
@@ -4,20 +4,24 @@ $defenseurs = @("Ramos", "Van Dijk", "De Ligt", "Ruben Dias", "Alaba", "Saliba",
 $milieux = @("Modric", "De Bruyne", "Kante", "Pogba", "Kroos", "Pedri", "Rodri")
 $attaquants = @("Messi", "Ronaldo", "Neymar", "Mbappé", "Lewandowski", "Benzema", "Haaland")
 
-# 1 gardien
+# Sélectionner un capitaine aléatoire
+$capitaine = $attaquants | Get-Random
+
+# 1 gardien pour l'équipe
 $gardienSelectionne = $gardiens | Get-Random
 
-# 4 défenseurs 
+# 4 défenseurs pour l'équipe
 $defenseursSelectionnes = $defenseurs | Get-Random -Count 4
 
-#3 milieux 
+# 3 milieux pour l'équipe
 $milieuxSelectionnes = $milieux | Get-Random -Count 3
 
-# 3 attaquants 
+# 3 attaquants pour l'équipe
 $attaquantsSelectionnes = $attaquants | Get-Random -Count 3
 
-# composition de l'équipe
+# Composition de l'équipe
 Write-Output "Ta composition d'equipe :"
+Write-Output "Capitaine : $capitaine"
 Write-Output "Gardien : $gardienSelectionne"
 Write-Output "Défenseurs : $($defenseursSelectionnes -join ', ')"
 Write-Output "Milieux : $($milieuxSelectionnes -join ', ')"
